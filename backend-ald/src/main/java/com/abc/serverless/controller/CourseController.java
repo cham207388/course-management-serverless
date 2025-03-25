e87c784916787f294689d8189901cf7aa3f28b51
Alhagie Bai Cham <cham207388@gmail.com>
2025-03-25 08:12:09 -0500

    versioning my api

--- backend-ald/src/main/java/com/abc/serverless/controller/CourseController.java
+++ backend-ald/src/main/java/com/abc/serverless/controller/CourseController.java
@@ -1,17 +1,26 @@
 package com.abc.serverless.controller;
 
-import com.abc.serverless.dto.Course;
-import com.abc.serverless.service.CourseService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abc.serverless.dto.Course;
+import com.abc.serverless.service.CourseService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/v1/courses")
 @RequiredArgsConstructor
 public class CourseController {
 
