52e2bda9021ed9f343e08ec83cd37fc4cf9b4834
Alhagie Bai Cham <cham207388@gmail.com>
2025-03-27 14:31:50 -0500

    correct cognito region

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
+output "aws_region" {
+  value = var.aws_region
+}
 output "api_invoke_url" {
   description = "Base URL to access the deployed API"
   value       = "https://${aws_api_gateway_rest_api.course_management.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.dev.stage_name}"
