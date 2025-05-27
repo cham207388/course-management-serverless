4e108790ee6c2d65863c2d25185bd4e3ceab8bf5
Alhagie Bai Cham <cham207388@gmail.com>
2025-05-27 16:42:12 -0500

    backend in us-east-2

--- terraform/settings.tf
+++ terraform/settings.tf
@@ -12,7 +12,7 @@ terraform {
   backend "s3" {
     bucket         = "project-terraform-state-abc"
     key            = "course-management/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "project-terraform-state-lock"
   }
 }
