1c77f0763e86828f0ee7a3547acf68e52984aba3
Alhagie Bai Cham <cham207388@gmail.com>
2025-06-03 14:07:43 -0500

    update imports

--- backend-ald/src/main/java/com/abc/serverless/Application.java
+++ backend-ald/src/main/java/com/abc/serverless/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 import com.abc.serverless.controller.PingController;
-import com.abc.serverless.config.CorsConfig;
 
 import jakarta.annotation.PostConstruct;
 
@@ -13,7 +12,7 @@ import jakarta.annotation.PostConstruct;
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "com.abc.serverless.controller")
-@Import({ PingController.class, CorsConfig.class })
+@Import({ PingController.class })
 public class Application {
 
     public static void main(String[] args) {
